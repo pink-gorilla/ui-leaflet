0985e055d1d6ff57f467f3397e6f9095792b5751
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-06-30 20:10:10 -0500

    moved from gorilla-ui

---
+++ script/clean.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm .cpcache -r
+rm node_modules -r
+rm target -r
+rm .webly -r
+rm .shadow-cljs -r
+
+rm package.json
+rm package-lock.json
+rm karma.conf.js
+rm shadow-cljs.edn
---
+++ src/deps.cljs
@@ -0,0 +1,10 @@
+{:npm-deps
+ {; shadow cljs creates package.json 
+  ; based on this dependencies 
+
+   ; leaflet maps
+  "leaflet" "^1.6.0"  ; icons
+  "react-leaflet" "^3.2.0"
+
+  ;  
+  }}
